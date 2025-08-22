a729592585cc1ae145a53e4b92e0bc721da854ec
Alam-Bangi <bangialam12@gmail.com>
2025-08-22 07:05:12 +0000

    22-August-2025

---
+++ Daily/August/22-August-2025/1.java
@@ -0,0 +1,40 @@
+public class FanRunner {
+    public static void main(String[] args) {
+        Fan fan = new Fan("M", 1, "Black");
+        fan.switchOn();
+        System.out.println(fan);
+        fan.setSpeed((byte)3);
+        System.out.println(fan);
+        fan.switchOff();
+        System.out.println(fan);
+    }
+}
+public class Fan {
+    private String make;
+    private double radius;
+    private String colour;
+    private boolean isOn;
+    private byte speed;
+    
+    public Fan (String make, double radius, String colour) {
+        this.make=make;
+        this.radius = radius;
+        this.colour = colour;
+    }
+    
+    public void switchOn() {
+        this.isOn= true;
+        setSpeed((byte)5);
+    }
+    public void switchOff() {
+        this.isOn= false;
+        setSpeed((byte)0);
+    }
+    public void setSpeed(byte speed) {
+        this.speed = speed;
+    }
+    public String toString() {
+        return String.format("make-%s , radius-%f , colour-%s , isOn-%b , speed-%d ", make, radius, colour, isOn, speed);
+    }
+}
+    
---
+++ Daily/August/22-August-2025/tasks.md
@@ -0,0 +1,4 @@
+## Tasks
+
+1. Class Fan and class FanRunner with instances of Fan class.
+2. Class Rectangle and class rectangleRunner with objects of Rectangle class.
