c7d55ebf2cef346349e9fc95e6bb747158f1cc96
Alam-Bangi <bangialam12@gmail.com>
2026-07-10 17:17:48 +0530

    Solved Assignment 6 of SE

--- Engineering/SE-1/Assignment6.java
+++ Engineering/SE-1/Assignment6.java
@@ -3,44 +3,66 @@ It is decided  that  weekly greetings are  to  be  furnished  to  wish the  stud
 birthdays in that week. The consolidated sorted list with desired categorical information 
 is to  be  provided  to  the  authority.  Write  a  Python  program  to  store  students  PRNs with 
 date  and  month  of  birth.  Let  List_A  and  List_B  be  the  two  list  for  two  SE  Computer 
-divisions.  Lists  are  sorted  on  date  and  month.  Merge  these  two  lists  into  third  list 
-“List_SE_Comp_DOB” resulting in sorted information about Date of Birth of SE Computer 
-students 
+divisions.  Lists  are  sorted  on  date  and  month.  Merge  these  two  lists  into  third  list List_SE_Comp_DOB 
+resulting in sorted information about Date of Birth of SE Computer students 
 */
 
-public class Assignment6 {
+import java.util.Arrays;
+
+class Assignment6 {
     public static void main(String[] args) {
         Student[] listA = {
-                new Student("PRN001", 15, 3),
-                new Student("PRN002", 22, 5),
-                new Student("PRN003", 10, 7)
+                new Student("PRN001", 15, 1),
+                new Student("PRN002", 22, 11),
+                new Student("PRN003", 10, 5)
         };
 
         Student[] listB = {
-                new Student("PRN004", 5, 2),
-                new Student("PRN005", 18, 4),
-                new Student("PRN006", 30, 6)
+                new Student("PRN004", 5, 3),
+                new Student("PRN005", 18, 9),
+                new Student("PRN006", 30, 7)
         };
 
+        Arrays.sort(listA);
+        Arrays.sort(listB);
         Student[] mergedList = mergeLists(listA, listB);
 
-        System.out.println("Merged List of Students (Sorted by Date of Birth):");
+        System.out.println("Merged List of Students (Sorted by Date of Birth/Month):");
 
+        for (Student student : mergedList) {
+            System.out.println("PRN: " + student.getPrn() + ", DOB: " + student.getDateOfBirth() + "/"
+                    + student.getMonthOfBirth());
+        }
     }
 
-    public static Student[] mergeLists(Student[] listA, Student[] listB) {
+    private static Student[] mergeLists(Student[] listA, Student[] listB) {
         int sizeA = listA.length;
         int sizeB = listB.length;
         Student[] mergedList = new Student[sizeA + sizeB];
 
-        int i = 0, j = 0, k = 0;
+        int listAindex = 0, listBindex = 0, mergedListindex = 0;
 
-        while (i < sizeA && j < sizeB) {
-
+        while (listAindex < sizeA && listBindex < sizeB) {
+            if (listA[listAindex].compareTo(listB[listBindex]) <= 0) {
+                mergedList[mergedListindex++] = listA[listAindex++];
+            } else {
+                mergedList[mergedListindex++] = listB[listBindex++];
+            }
         }
-    }
 
-class Student {
+        while (listAindex < sizeA) {
+            mergedList[mergedListindex++] = listA[listAindex++];
+        }
+
+        while (listBindex < sizeB) {
+            mergedList[mergedListindex++] = listB[listBindex++];
+        }
+
+        return mergedList;
+    }
+}
+
+class Student implements Comparable<Student> {
     private String prn;
     private int dateOfBirth;
     private int monthOfBirth;
@@ -50,6 +72,7 @@ class Student {
         this.dateOfBirth = dateOfBirth;
         this.monthOfBirth = monthOfBirth;
     }
+
     public String getPrn() {
         return prn;
     }
@@ -60,5 +83,14 @@ class Student {
 
     public int getMonthOfBirth() {
         return monthOfBirth;
+    }
+
+    @Override
+    public int compareTo(Student other) {
+        if (this.monthOfBirth != other.monthOfBirth) {
+            return Integer.compare(this.monthOfBirth, other.monthOfBirth);
+        } else {
+            return Integer.compare(this.dateOfBirth, other.dateOfBirth);
+        }
     }
 }
